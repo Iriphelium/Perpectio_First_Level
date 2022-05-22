e0db3d0e9a95e2d98cfe006c6b12284d56032c36
Iriphelium <altavega0810@gmail.com>
2022-05-22 11:38:08 +0200

    Main

---
+++ Perspectio-main/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float jumpforce;
+    public float moveSpeed;
+    Rigidbody rb;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask ground;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        rb.velocity = new Vector3(horizontalInput * moveSpeed, rb.velocity.y, verticalInput * moveSpeed);
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpforce, rb.velocity.z);
+        }
+
+    }
+
+        bool IsGrounded()
+        { 
+            return Physics.CheckSphere(groundCheck.position,.1f,ground); 
+        
+        }
+    }
